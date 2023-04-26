018dbd63291587254bcab7b68ab56bfe4bd0f1de
Kat-Devs <katlegophele95@gmail.com>
2023-04-26 12:59:18 +0200

    0X0B Task 0 and 4 update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,12 @@
-# Changes SSH config file
-exec { 'echo':
-  path    => 'usr/bin:/bin',
-  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> ~/.ssh/ssh_config',
-  returns => [0,1],
+# Puppet script to create ssh config file
+file_line { 'Diable passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Path to identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
