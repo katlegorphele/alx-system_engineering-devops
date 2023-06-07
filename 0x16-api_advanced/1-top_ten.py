57b5511aafd7c097d0334d04bf50caacdb007337
Kat-Devs <katlegophele95@gmail.com>
2023-06-07 12:18:01 +0200

    0x16 - Task 1 Update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python3
-
 """
 Function that queries the Reddit API
 validates a subreddit and
 retrieves the top 10 hot posts
 """
-
 import requests
 
 
 def top_ten(subreddit):
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    """Get top ten posts"""
+    # Get url
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    # Get headers
     headers = {"User-Agent": "My User Agent 1.0"}
-
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        posts = data["data"]["children"]
-
-        if len(posts) > 0:
-            print(f"Top 10 hot posts in /r/{subreddit}:")
-            for post in posts:
-                print(post["data"]["title"])
-        else:
-            print(f"No hot posts found in /r/{subreddit}.")
-    else:
-        print("None")
+    if response.status_code != 200:
+        print(None)
+        return
+    posts = response.json().get("data").get("children")
+    for post in posts[:10]:
+        print(post.get("data").get("title"))
