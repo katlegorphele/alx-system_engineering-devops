b340dd347e390cc3b604ca072892480ff82ae109
Kat-Devs <katlegophele95@gmail.com>
2023-08-08 22:00:17 +0200

    0x16 - 0-subs fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-
-"""
-Function that queries the Reddit API and returns the number of subscribers
-"""
+"""Module that quieries Reddit API,returns the number of subscribers"""
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """
-    Function that queries the Reddit API and returns the number of subscribers
-    """
-    # Set the URL
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    # Set the headers
-    headers = {"User-Agent": "My User Agent 1.0"}
+    """Queries the Reddit API and returns the number of subscribers (not
+    active users, total subscribers) for a given subreddit.
 
-    # Make the request
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    Returns:
+    int: number of subscribers
+    """
+    base_url = 'https://www.reddit.com/r/'
 
-    # If the status code is 200, return the number of subscribers
-    if response.status_code == 200:
-        return response.json().get("data").get("subscribers")
-    else:
-        return 0
+    url = '{}{}/about.json'.format(base_url, subreddit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    results = requests.get(
+        url,
+        headers=headers,
+        allow_redirects=False
+    )
+    if results.status_code == 200:
+        return results.json()['data']['subscribers']
+    return 0
